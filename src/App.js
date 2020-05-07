8c9caaa4c727a41543f6384647c17f2c48b914f9
georgelzeng <georgelzeng@berkeley.edu>
2020-05-07 13:45:47 -0700

    removing extra imports

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import SectionOne from './sections/SectionOne'
 import SectionTwo from './sections/SectionTwo'
 import SectionThree from './sections/SectionThree'
 import SectionFour from './sections/SectionFour'
-import Footer from './components/Footer'
 
 
 
 class App extends React.Component {
+
+  componentWillMount() {
+    document.title = 'George Zeng'
+  }
+
   render() {
     return (
       <div className="App">
